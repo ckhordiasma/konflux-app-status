3caeb7bc98141b780e5ae09c16eb650d6f038a11
Christopher Kodama <ckodama@redhat.com>
2024-12-23 16:05:21 -0500

    updates to script.sh and script.py
    made script.py focus solely on downselecting from 200+ pipelineruns to just the most recent.
    
    script.sh now does both the colorizing of status and making the hyperlinks

--- script.py
+++ script.py
@@ -2,16 +2,6 @@ import sys
 import re
 import json
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 components = {name: True for name in sys.argv[1].split("\n")}
 pipelines = json.loads(sys.argv[2])
@@ -22,20 +12,12 @@ for entry in pipelines:
   for component in components:
     if re.match(component, pipeline):
       # print(f"{pipeline} matches {component}")
-      most_recent_pipelines[component] = [pipeline, status]
+      most_recent_pipelines[component] = {"pipeline": pipeline, "status":status}
       components.pop(component) 
       break
 
-for component in most_recent_pipelines:
-  pipeline, status = most_recent_pipelines[component]
-  if re.match('Failed',status):
-    status_color = bcolors.FAIL
-  elif re.match('Running',status):
-    status_color = bcolors.OKBLUE
-  else:
-    status_color = bcolors.OKGREEN
-  print(f"{pipeline}\t{status_color}{status}{bcolors.ENDC}")
+
+print(json.dumps(most_recent_pipelines))
 
 if len(components) != 0:
-  print("some components were not found in pipeline history:")
-  print(",".join([c for c in components]))
+  raise ValueError(f"some components were not found in pipeline history:{",".join([c for c in components])}")
--- script.sh
+++ script.sh
@@ -9,19 +9,31 @@ echo "getting pipelines for $APP..."
 # event-type can be 'incoming' or 'push' for valid builds, so I am using a filter for != pull_request instead
 # < /dev/null is needed to stop the script from being interactive
 
-APP_URL="https://konflux.apps.stone-prod-p02.hjvn.p1.openshiftapps.com/application-pipeline/workspaces/rhoai/applications/$APP"
-
-kubectl tekton get pr --labels="appstudio.openshift.io/application=$APP" \
+pipelines=$(kubectl tekton get pr --labels="appstudio.openshift.io/application=$APP" \
     --filter="data.metadata.labels['pipelinesascode.tekton.dev/event-type'] != 'pull_request'" \
-    --limit 100 < /dev/null \
+    --limit 100 <&- \
     | sed 's/^Next Page.*//' | sed 's/^NAME *UID.*//' | awk NF \
-    | awk -F '  +' '
-      BEGIN{printf "["} 
-      END{printf "]"}
-      {printf "%s{\"pipeline\": \"%s\", \"status\":\"%s\"}",separator, $1, $5; separator=","}' \
-    | read -r pipelines
+    | awk -F '  +' 'BEGIN{printf "["} END{printf "]"} {printf "%s{\"pipeline\": \"%s\", \"status\":\"%s\"}",separator, $1, $5; separator=","}')
+echo "processing list of pipelines..."
+current_pipelines=$(python3 script.py "$components" "$pipelines")
 
+sample_pipeline=$(echo $current_pipelines | jq -r '.[].pipeline' | head -n 1)
 
-    python3 script.py "$components" "$pipelines"  \
-    | sed -E "s|^([^[:space:]]+)|\1 ${APP_URL}/pipelineruns/\1/logs|" \
-    | sed -E 's|^([^[:space:]]+) ([^[:space:]]+)|\x1B]8;;\2\x1B\\\1\x1B]8;;\x1B\\|'
+APP_URL=$(kubectl tekton get pr $sample_pipeline -o jsonpath='{.metadata.annotations}' | jq -r '.["pipelinesascode.tekton.dev/log-url"]' | sed -E 's|(.*)/.*$|\1|')
+
+echo "formatting output..."
+FINAL_OUTPUT=""
+pipelines_list=$(echo "$current_pipelines" | jq -rc '.[]')
+for pipeline in $pipelines_list; do
+  pr_name=$(echo "$pipeline" | jq -r '.pipeline')
+  pr_status=$(echo "$pipeline" | jq -r '.status')
+  FINAL_OUTPUT="${FINAL_OUTPUT}\n$pr_name $pr_status"
+done
+
+echo "adding color and hyperlinks..."
+
+echo -e "$FINAL_OUTPUT" | column -t \
+  | sed -E 's|^([^ ]*)|\x1B]8;;'"$APP_URL/"'\1\x1B\\\1\x1B]8;;\x1B\\|' \
+  | sed -E 's|(Succeeded.*)$|\x1B[92m\1\x1B[0m|' \
+  | sed -E 's|(Failed.*)$|\x1B[91m\1\x1B[0m|' \
+  | sed -E 's|(Running.*)$|\x1B[94m\1\x1B[0m|' 
